00ab6ff5e0ec39a28ec05fe6a18abb107b3d9cf9
SonnieCodes <mgathoni.gathoni9@gmail.com>
2025-02-16 19:18:42 +0300

    regex

--- lib/regex.py
+++ lib/regex.py
@@ -1,13 +1,15 @@
 import re
 
-# NOTE: There are only a few tests included, so multiple solutions will work.
+# # NOTE: There are only a few tests included, so multiple solutions will work.
 # Feel free to encourage students to find oversights and add tests to this lab!
 
-name = r""
-name_regex = re.compile(name)
+# name = r"^[A-Z][a-z]+\s[A-Z][a-z]+$\."
+name_regex = re.compile(r"^[A-Z][a-z]*(?:'[A-Z][a-z]*)?(?:\s[A-Z][a-z]+(?:-[A-Z][a-z]+)?)?$")
 
-phone_number = r""
-phone_regex = re.compile(phone_number)
 
-email_address = r""
-email_regex = re.compile(email_address)
+# phone_number = r"^\(?\d{3}\)?[-.\s]?\d{3}[-.\s]?\d{4}$\."
+phone_regex = re.compile(r"^\(?\d{3}\)?[-.\s]?\d{3}[-.\s]?\d{4}$")
+
+# email_address = r"^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$\."
+email_regex = re.compile(r"^[a-zA-Z][a-zA-Z0-9_.+-]*@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$")
+
